4f4642b8be3b00b508aed28bbef957f6aad5b6e8
tylermcleant <tylermcleant@aol.com>
2020-07-27 03:01:36 -0400

    getting started

---
+++ greeting.js
@@ -0,0 +1,6 @@
+module.exports.greeting1 = "Hello, welcome to the greatest team ever at TM Industries Co.!";
+console.log(this.greeting1);
+module.exports.greetingspc = "";
+console.log(this.greetingspc);
+module.exports.greetingln2 = "If this is your first time logging in, please answer the following:";
+console.log(this.greetingln2);
---
+++ userInput.js
@@ -0,0 +1,9 @@
+var fs = require("fs");
+var inq = require("inquirer");
+var greeting = require("./greeting");
+
+
+
+
+
+
