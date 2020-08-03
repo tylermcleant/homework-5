ff9072225050c10bd944f059df42c061658fc3b7
tylermcleant <tylermcleant@aol.com>
2020-08-03 01:10:52 -0400

    updated iteration

---
+++ README.md
@@ -0,0 +1,2 @@
+# GIF for the Server Demonstration 
+
--- userInput.js
+++ userInput.js
@@ -1,9 +1,43 @@
 var fs = require("fs");
-var inq = require("inquirer");
 var greeting = require("./greeting");
+const inquirer = require("inquirer");
 
-
-
-
-
-
+function userQuestions() { 
+// Series of questions to get username and password information. 
+inquirer
+.prompt([
+    {
+    type: "input",
+    message: "What is your user name?",
+    name: "username"
+    },
+    {
+    type: "password",
+    message: "What is your password?",
+    name: "password"
+    },
+    {
+    type: "password",
+    message: "Re-enter password to confirm:",
+    name: "confirm"
+    }
+])
+.then(function(data) {
+    if (data.password !== data.confirm){ // Checker to ensure the same password was keyed each time. 
+    console.log("Passwords do not match, Please re-enter your information");
+    userQuestions ();
+    }else{
+    var fs = require("fs");
+// Storing needed for the userPassword.txt file per requirements. 
+    fs.writeFile("./userPassword.txt",data.username + " : " + data.password,
+function(err) {
+    if (err) {
+        return console.log(err);
+    }else{
+        console.log("Success!");
+    }
+});
+}
+});
+}
+userQuestions();
